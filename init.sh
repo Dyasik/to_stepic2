d9f6334ccf6dcf0a7430d619c5ae042591b9f6ec
Denis Seletkov <rysds2@gmail.com>
2016-06-04 23:18:18 +0400

    11

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ln -s /home/box/web/gunicorn.conf.py   /etc/gunicorn.d/default
+sudo ln -sf /home/box/web/gunicorn.conf.py   /etc/gunicorn.d/default
 sudo gunicorn -c /home/box/web/hello
 sudo ln -sf /home/box/web/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
