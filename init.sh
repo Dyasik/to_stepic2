1ee4ffb358307fff91280b5617213c3d77d50090
Denis Seletkov <rysds2@gmail.com>
2016-06-04 23:08:17 +0400

    8

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sudo gunicorn -b 0.0.0.0:8080 /home/box/web/hello
+sudo gunicorn -c 0.0.0.0:8080 /home/box/web/hello
 sudo ln -sf /home/box/web/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
