c59a2fd6d06fc80812f2317d5d70806b40cc0d2e
Denis Seletkov <rysds2@gmail.com>
2016-06-07 13:58:52 +0400

    15

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ def wsgi_application(environ, start_response):
         ('Content-Type', 'text/plain')
     ]
     start_response(status, headers)
-    print(environ)
-    return environ[0]
-    # return ["\n".join(environ[QUERY_STRING].split("&"))]
+    # print(environ)
+    # return environ[0]
+    return ["\n".join(environ["QUERY_STRING"].split("&"))]
