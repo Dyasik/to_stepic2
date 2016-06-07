f1af4b332042b8459729ca9aed87c3cd8e26e470
Denis Seletkov <rysds2@gmail.com>
2016-06-07 13:46:54 +0400

    14

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_application &
 sudo ln -sf /home/box/web/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_application &
